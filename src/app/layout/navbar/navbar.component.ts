dbfd769fc0baa8dda7fbd789ed698d0c70f8b19c
Anselmo Ortega <lfkanselmo@gmail.com>
2026-04-28 20:16:11 -0500

    fix: ajustes responsivos para mobiles

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { AuthService } from '../../core/services/auth.service';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-navbar',
@@ -15,14 +16,15 @@ import { AuthService } from '../../core/services/auth.service';
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
+    MatDividerModule,
   ],
   templateUrl: './navbar.component.html',
-  styleUrl:    './navbar.component.scss'
+  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
   private authService = inject(AuthService);
-  private router      = inject(Router);
+  private router = inject(Router);
 
   get userName(): string | null {
     return this.authService.getUserName();
